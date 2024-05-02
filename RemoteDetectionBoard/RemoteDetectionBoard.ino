9315b34d7f771f40244c36d7aadf6b146950f182
HWAN <68631787+hlam812003@users.noreply.github.com>
2024-05-02 23:36:11 +0700

    Update RemoteDetectionBoard.ino

--- RemoteDetectionBoard/RemoteDetectionBoard.ino
+++ RemoteDetectionBoard/RemoteDetectionBoard.ino
@@ -1,5 +1,9 @@
 #include <Wire.h>
 #include <WiFi.h>
+#include <TinyGPS++.h>
+
+TinyGPSPlus gps;
+HardwareSerial serialGPS(2);
 
 const int MPU_addr = 0x68;  // I2C address of the MPU-6050
 int16_t AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
@@ -12,16 +16,18 @@ byte trigger1count = 0;
 byte trigger2count = 0;
 byte trigger3count = 0;
 int angleChange = 0;
+const int buzzerPin = 18;
 
-const char *ssid = "qweq"; // SSID của WIFI
-const char *pass = "123"; // Password của WiFi
+const char *ssid = "QUOC HUNG LAU 1"; // SSID của WIFI
+const char *pass = "@Lamvanha25011973"; // Password của WiFi
 
-void send_event(const char *event);
+void send_event(const char *event, const char *details);
 const char *host = "maker.ifttt.com";
 const char *privateKey = "c3IEHJY8SL8qK3zcfznXDt";
 
 void setup() {
   Serial.begin(115200);
+  serialGPS.begin(9600, SERIAL_8N1, 16, 17);
   Wire.begin();
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x6B);
@@ -35,135 +41,144 @@ void setup() {
   }
   Serial.println("");
   Serial.println("WiFi is connected!!!");
+
+  pinMode(buzzerPin, OUTPUT);
 }
 
 void loop() {
-  mpu_read();
-  ax = (AcX - 2050) / 16384.00;
-  ay = (AcY - 77) / 16384.00;
-  az = (AcZ - 1947) / 16384.00;
-  gx = (GyX + 270) / 131.07;
-  gy = (GyY - 351) / 131.07;
-  gz = (GyZ + 136) / 131.07;
-
-  float raw_amplitude = pow(pow(ax, 2) + pow(ay, 2) + pow(az, 2), 0.5);
-  int amplitude = raw_amplitude * 10;
-  Serial.println(amplitude);
-
-  if (amplitude <= 3 && trigger2 == false) {
-    trigger1 = true;
-    Serial.println("TRIGGER 1 ACTIVATED");
-  }
-
-  if (trigger1 == true) {
-    trigger1count++;
-    if (amplitude >= 10) {
-      trigger2 = true;
-      Serial.println("TRIGGER 2 ACTIVATED");
-      trigger1 = false;
-      trigger1count = 0;
+    while (serialGPS.available() > 0) {
+        gps.encode(serialGPS.read());
     }
-  }
 
-  if (trigger2 == true) {
-    trigger2count++;
-    angleChange = pow(pow(gx, 2) + pow(gy, 2) + pow(gz, 2), 0.5);
-    Serial.println(angleChange);
+    mpu_read();
+    ax = (AcX - 2050) / 16384.00;
+    ay = (AcY - 77) / 16384.00;
+    az = (AcZ - 1947) / 16384.00;
+    gx = (GyX + 270) / 131.07;
+    gy = (GyY - 351) / 131.07;
+    gz = (GyZ + 136) / 131.07;
 
-    if (angleChange >= 50 && angleChange <= 300) {
-      trigger3 = true;
-      trigger2 = false;
-      trigger2count = 0;
-      Serial.println(angleChange);
-      Serial.println("TRIGGER 3 ACTIVATED");
+    float raw_amplitude = pow(pow(ax, 2) + pow(ay, 2) + pow(az, 2), 0.5);
+    int amplitude = raw_amplitude * 10;
+    Serial.println(amplitude);
+
+    if (amplitude <= 3 && trigger2 == false) {
+        trigger1 = true;
+        Serial.println("TRIGGER 1 ACTIVATED");
     }
-  }
 
-  if (trigger3 == true) {
-    trigger3count++;
-    if (trigger3count >= 10) {
-      angleChange = pow(pow(gx, 2) + pow(gy, 2) + pow(gz, 2), 0.5);
-      Serial.println(angleChange);
+    if (trigger1 == true) {
+        trigger1count++;
+        if (amplitude >= 10) {
+            trigger2 = true;
+            Serial.println("TRIGGER 2 ACTIVATED");
+            trigger1 = false;
+            trigger1count = 0;
+        }
+    }
 
-      if ((angleChange >= 0) && (angleChange <= 15)) {
-        fall = true;
-        trigger3 = false;
-        trigger3count = 0;
+    if (trigger2 == true) {
+        trigger2count++;
+        angleChange = pow(pow(gx, 2) + pow(gy, 2) + pow(gz, 2), 0.5);
         Serial.println(angleChange);
 
-        char details[300];
-        snprintf(details, 300, "Acceleration: ax=%.2f, ay=%.2f, az=%.2f",
-                 ax, ay, az, millis() / 1000);
-
-        send_event("FALL DETECTION", details);
-      } else {
-        trigger3 = false;
-        trigger3count = 0;
-        Serial.println("TRIGGER 3 DEACTIVATED");
-      }
+        if (angleChange >= 50 && angleChange <= 300) {
+            trigger3 = true;
+            trigger2 = false;
+            trigger2count = 0;
+            Serial.println(angleChange);
+            Serial.println("TRIGGER 3 ACTIVATED");
+        }
     }
-  }
 
-  if (fall == true) {
-    Serial.println("FALL DETECTED");
-    fall = false;
-  }
+    if (trigger3 == true) {
+        trigger3count++;
+        if (trigger3count >= 10) {
+            angleChange = pow(pow(gx, 2) + pow(gy, 2) + pow(gz, 2), 0.5);
+            Serial.println(angleChange);
 
-  if (trigger2count >= 5) {
-    trigger2 = false;
-    trigger2count = 0;
-    Serial.println("TRIGGER 2 DECACTIVATED");
-  }
+            if ((angleChange >= 0) && (angleChange <= 15)) {
+                fall = true;
+                trigger3 = false;
+                trigger3count = 0;
+                Serial.println("FALL DETECTED");
 
-  if (trigger1count >= 5) {
-    trigger1 = false;
-    trigger1count = 0;
-    Serial.println("TRIGGER 1 DECACTIVATED");
-  }
-  delay(100);
+                digitalWrite(buzzerPin, HIGH); // Kích hoạt còi
+                delay(3000); // Cho còi kêu trong 3 giây
+                digitalWrite(buzzerPin, LOW); // Tắt còi
+
+                char details[500];
+                if (gps.location.isValid()) {
+                    snprintf(details, 500, "Latitude: %.6f, Longitude: %.6f",
+                            gps.location.lat(), gps.location.lng());
+                } else {
+                    snprintf(details, 500, "GPS signal not available");
+                }
+
+                send_event("FALL DETECTION", details);
+            } else {
+                trigger3 = false;
+                trigger3count = 0;
+                Serial.println("TRIGGER 3 DEACTIVATED");
+            }
+        }
+    }
+
+    if (trigger2count >= 5) {
+        trigger2 = false;
+        trigger2count = 0;
+        Serial.println("TRIGGER 2 DECACTIVATED");
+    }
+
+    if (trigger1count >= 5) {
+        trigger1 = false;
+        trigger1count = 0;
+        Serial.println("TRIGGER 1 DECACTIVATED");
+    }
+    delay(100);
 }
 
 void mpu_read() {
-  Wire.beginTransmission(MPU_addr);
-  Wire.write(0x3B);
-  Wire.endTransmission(false);
-  Wire.requestFrom((uint8_t)MPU_addr, (size_t)14, (bool)true); 
-  AcX = Wire.read() << 8 | Wire.read();  
-  AcY = Wire.read() << 8 | Wire.read();  
-  AcZ = Wire.read() << 8 | Wire.read();  
-  Tmp = Wire.read() << 8 | Wire.read();  
-  GyX = Wire.read() << 8 | Wire.read();  
-  GyY = Wire.read() << 8 | Wire.read();  
-  GyZ = Wire.read() << 8 | Wire.read();  
+    Wire.beginTransmission(MPU_addr);
+    Wire.write(0x3B);
+    Wire.endTransmission(false);
+    Wire.requestFrom((uint8_t)MPU_addr, (size_t)14, (bool)true); 
+    AcX = Wire.read() << 8 | Wire.read();  
+    AcY = Wire.read() << 8 | Wire.read();  
+    AcZ = Wire.read() << 8 | Wire.read();  
+    Tmp = Wire.read() << 8 | Wire.read();  
+    GyX = Wire.read() << 8 | Wire.read();  
+    GyY = Wire.read() << 8 | Wire.read();  
+    GyZ = Wire.read() << 8 | Wire.read();  
 }
 
 void send_event(const char *event, const char *details) {
-  Serial.print("Connecting to ");
-  Serial.println(host);
-  WiFiClient client;
-  const int httpPort = 80;
-  if (!client.connect(host, httpPort)) {
-    Serial.println("Connection failed");
-    return;
-  }
-  String url = "/trigger/";
-  url += event;
-  url += "/with/key/";
-  url += privateKey;
-  url += "?value1=";
-  url += details;
-  Serial.print("Requesting URL: ");
-  Serial.println(url);
-  client.print(String("GET ") + url + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "Connection: close\r\n\r\n");
-  while (client.connected()) {
-    if (client.available()) {
-      String line = client.readStringUntil('\r');
-      Serial.print(line);
-    } else {
-      delay(50);
-    };
-  }
-  Serial.println();
-  Serial.println("closing connection");
-  client.stop();
+    Serial.print("Connecting to ");
+    Serial.println(host);
+    WiFiClient client;
+    const int httpPort = 80;
+    if (!client.connect(host, httpPort)) {
+        Serial.println("Connection failed");
+        return;
+    }
+    String url = "/trigger/";
+    url += event;
+    url += "/with/key/";
+    url += privateKey;
+    url += "?value1=";
+    url += details;
+    Serial.print("Requesting URL: ");
+    Serial.println(url);
+    client.print(String("GET ") + url + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "Connection: close\r\n\r\n");
+    while (client.connected()) {
+        if (client.available()) {
+            String line = client.readStringUntil('\r');
+            Serial.print(line);
+        } else {
+            delay(50);
+        }
+    }
+    Serial.println();
+    Serial.println("closing connection");
+    client.stop();
 }
